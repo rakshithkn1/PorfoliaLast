af94e2056c239cbbe653283c69bb8238f63cec25
rakshithkn1 <rakshithkn2001@gmail.com>
2024-03-23 12:25:05 +0530

    completed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 
 export default function Footer() {
     return (
-        <footer className="bg-white border-y" style={{position:'relative' ,top:'100px'}}>
+        <footer className="bg-white border-y" style={{position:'relative' ,top:'100px',marginBottom:'-800px'}}>
             <div className="mx-auto w-full max-w-screen-xl p-4 py-0 lg:py-8" >
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0" >
