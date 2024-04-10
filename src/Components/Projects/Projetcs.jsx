9a94241e232b0eefd2157933d1403b33965584e3
rakshithkn1 <rakshithkn2001@gmail.com>
2024-04-10 17:35:47 +0530

    Update Projects and Contact components

--- src/Components/Projects/Projetcs.jsx
+++ src/Components/Projects/Projetcs.jsx
@@ -5,10 +5,14 @@ function Projetcs() {
   return (
     
     <div className="flex justify-center items-center gap-3 mt-5 ">
-      <div className="h-[60vh] w-[40vw] border uppercase text-2xl text-center flex flex-col gap-5 text-green-500 bg-gray-200">Projects
+      <div className="h-[70vh] w-[40vw] border uppercase text-2xl text-center flex flex-col gap-5 text-green-500 bg-gray-200">Projects
       <div className="text-blue-700 mt-8">  
                        
-                            <div className="h-3 w-3  bg-#84cc16 inline-block rounded-full mr-2"></div> <div className="mb-8 inline-block">   Random Password Generator   </div>
+                            <div className="h-3 w-3  bg-#84cc16 inline-block rounded-full mr-2"></div> 
+                            <div className="mb-8 inline-block">   Quiz App   </div>
+
+                            <div className="mb-8  pl-[-5vw]">   Currency Converter of different countries  </div>
+                            <div className="mb-8  pl-[-5vw]">   Random Password Generator   </div>
                                <div className="gap-5 pl-[-4vw] mb-8" > BackGround Color Changer  </div>
                             <div className="gap-5 pl-[5vw] mb-8" >   Animated website for Forest Saving Awareness</div> 
               
@@ -16,9 +20,11 @@ function Projetcs() {
                                
                       </div>
       </div>
-      <div className="h-[60vh] w-[40vw] border uppercase text-2xl text-center flex flex-col gap-5 text-green-500 bg-gray-200">Deployed Links
+      <div className="h-[70vh] w-[40vw] border uppercase text-2xl text-center flex flex-col gap-5 text-green-500 bg-gray-200">Deployed Links
       <div className="text-gray-500 mt-8">  
                        
+                             <div className="mb-8">   <a href="https://quizappmajorproject.netlify.app/" >Quiz App  </a></div>
+                             <div className="mb-8">   <a href="https://currencyconverterusingreactsspeacial.netlify.app/" >Currency Converter of different countries   </a></div>
                              <div className="mb-8">   <a href="https://reacordkeepingapplication.netlify.app/" >Random Password Generator   </a></div>
                                <div className="gap-5 pl-[-4vw] mb-8" > <a href="https://backgroundchangerapplicationusingreac.netlify.app/" >BackGround Color Changer  </a></div>
                             <div className="gap-5 pl-[5vw] mb-8" >   <a href="https://animated-website-of-saving-forest.netlify.app/" >Animated website for Forest Saving Awareness</a></div> 
--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -11,14 +11,14 @@ import { FaPhoneVolume } from "react-icons/fa";
 
 
 export default function Contact() {
-    const emailAddress = "rakshithkn2001@gmail.com"
+    const emailAddress = "knrakshith45@gmail.com"
     return (
         <div style={{padding:'40px'}}>
 <h1 style={{textAlign:'center',fontSize:'30px',color:'#1F259B '}} className='skills'>Contact Details</h1>
 
         <div style={{ display:"flex", justifyContent:'center',padding:"10px"}}>
         <div className='name' style={{height:'400px', width:'400px', border:'2px solid gray',borderRadius:'20px',display:'flex',justifyContent:'space-around',flexDirection:'column',position:'relative'}}>
-<div style={{position:'relative',left:'55px',padding:'20px' ,display:'inline',fontWeight:'bold'}}><CgMail  style={{display:'inline'}}/>  :  <a className='dark:md:hover:text-fuchsia-600'   href={`mailto:${emailAddress}` }>rakshithkn2001@gmail.com</a> </div>
+<div style={{position:'relative',left:'55px',padding:'20px' ,display:'inline',fontWeight:'bold'}}><CgMail  style={{display:'inline'}}/>  :  <a className='dark:md:hover:text-fuchsia-600'   href={`mailto:${emailAddress}` }>knrakshith45@gmail.com</a> </div>
 <div style={{display:'inline',position:'relative',left:'55px',padding:'20px',fontWeight:'bold'}}>  <FaLinkedin style={{display:'inline'}} />   :<a className='dark:md:hover:text-fuchsia-600'   href="https://www.linkedin.com/in/rakshithkn27/" style={{display:'inline' ,color:'indigo', }}>Click Here For Linkedin </a> </div>
 <div style={{display:'inline',position:'relative',left:'55px',padding:'20px',fontWeight:'bold'}}>  <FaGithub style={{display:'inline'}}/> :<a className='dark:md:hover:text-fuchsia-600'   href="https://github.com/rakshithkn1" style={{display:'inline' ,color:'indigo', }}>Click Here For Github </a> </div>
 <div style={{display:'inline',position:'relative',left:'55px',padding:'20px',fontWeight:'bold'}}> <FaPhoneVolume style={{display:'inline'}}/> :<a className='dark:md:hover:text-fuchsia-600'   href="9353763916" style={{display:'inline' ,color:'indigo', }}>+91-9353763916</a> </div>
